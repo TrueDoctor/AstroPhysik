5294bab8b8a3ab73080287865e5c85de9f0f3650
Dennis Kobert <d-kobert@web.de>
2016-09-10 01:05:42 +0200

    initial Changes

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort myport = new SerialPort();
+            myport.BaudRate = 9600;
+            myport.PortName = "COM5";
+            myport.Open();
+
+            while (true)
+            {
+                string data = myport.ReadLine();
+                Console.WriteLine(data);
+            }
+
+        }
+    }
+}
