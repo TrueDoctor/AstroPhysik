1ef12d274fba9ffa4cc43588436f6cddfe63462c
TrueDoctor <d-kobert@web.de>
2016-09-14 22:10:30 +0200

    Uploaded c# project

---
+++ LightControll/LightControll/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace LightControll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Bar = new System.Windows.Forms.VScrollBar();
+            this.myport = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Bar
+            // 
+            this.Bar.Location = new System.Drawing.Point(121, 91);
+            this.Bar.Maximum = 264;
+            this.Bar.Name = "Bar";
+            this.Bar.Size = new System.Drawing.Size(17, 47);
+            this.Bar.TabIndex = 0;
+            this.Bar.Value = 10;
+            this.Bar.ValueChanged += new System.EventHandler(this.vScrollBar1_ValueChanged);
+            // 
+            // myport
+            // 
+            this.myport.PortName = "COM3";
+            this.myport.RtsEnable = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Bar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.VScrollBar Bar;
+        private System.IO.Ports.SerialPort myport;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
