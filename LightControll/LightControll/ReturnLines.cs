093d7b4c53bee155d811866a67c1f0dd8bdd7f94
Dennis Kobert <d-kobert@web.de>
2016-09-19 22:18:32 +0200

    haha

--- LightControll/LightControll/ReturnLines.cs
+++ LightControll/LightControll/ReturnLines.cs
@@ -17,24 +17,24 @@ namespace LightControl
 
             string[] codeRawArray = codeRaw.Split('\n');
 
-            for (int i = 0; i < codeRawArray.Length; i++)
+            foreach (string line in codeRawArray)
             {
-                int index = codeRawArray[i].IndexOf("@");
+                int index = line.IndexOf("@");
                 if (index >= 0)
                 {
-                    switch (codeRawArray[i][index + 1])
+                    switch (line[index + 1])
                     {
                         case '0': codeRawArray[i].Replace("@0", Path.Combine(dir, "amcharts.js")); break;
-                        case '1': codeRawArray[i].Replace("@0", Path.Combine(dir, "serial.js")); break;
-                        case '2': codeRawArray[i].Replace("@0", Path.Combine(dir, "plugins", "export", "export.min.js")); break;
-                        case '3': codeRawArray[i].Replace("@0", Path.Combine(dir, "plugins", "export", "export.css")); break;
-                        case '4': codeRawArray[i].Replace("@0", Path.Combine(dir, "themes", "light.js")); break;
-                        case '5': codeRawArray[i].Replace("@0", brightness[0].ToString()); break;
-                        case '6': codeRawArray[i].Replace("@0", brightness[1].ToString()); break;
-                        case '7': codeRawArray[i].Replace("@0", brightness[2].ToString()); break;
-                        case '8': codeRawArray[i].Replace("@0", brightness[3].ToString()); break;
-                        case '9': codeRawArray[i].Replace("@0", brightness[4].ToString()); break;
-                        case 'A': codeRawArray[i].Replace("@0", brightness[5].ToString()); break;
+                        case '1': codeRawArray[i].Replace("@1", Path.Combine(dir, "serial.js")); break;
+                        case '2': codeRawArray[i].Replace("@2", Path.Combine(dir, "plugins", "export", "export.min.js")); break;
+                        case '3': codeRawArray[i].Replace("@3", Path.Combine(dir, "plugins", "export", "export.css")); break;
+                        case '4': codeRawArray[i].Replace("@4", Path.Combine(dir, "themes", "light.js")); break;
+                        case '5': codeRawArray[i].Replace("@5", brightness[0].ToString()); break;
+                        case '6': codeRawArray[i].Replace("@6", brightness[1].ToString()); break;
+                        case '7': codeRawArray[i].Replace("@7", brightness[2].ToString()); break;
+                        case '8': codeRawArray[i].Replace("@8", brightness[3].ToString()); break;
+                        case '9': codeRawArray[i].Replace("@9", brightness[4].ToString()); break;
+                        case 'A': codeRawArray[i].Replace("@A", brightness[5].ToString()); break;
                     }
                 }
             }
