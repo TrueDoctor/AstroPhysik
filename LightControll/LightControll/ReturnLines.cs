9ac5f87977127c24c0ced0b598a35883cbb1bcf4
TrueKuehli <rctcoaster2000@hotmail.de>
2016-09-19 22:14:46 +0200

    Added Code Replacements

--- LightControll/LightControll/ReturnLines.cs
+++ LightControll/LightControll/ReturnLines.cs
@@ -19,16 +19,26 @@ namespace LightControl
 
             for (int i = 0; i < codeRawArray.Length; i++)
             {
-                if (codeRawArray[i].IndexOf("{*}"))
+                int index = codeRawArray[i].IndexOf("@");
+                if (index >= 0)
                 {
-
+                    switch (codeRawArray[i][index + 1])
+                    {
+                        case '0': codeRawArray[i].Replace("@0", Path.Combine(dir, "amcharts.js")); break;
+                        case '1': codeRawArray[i].Replace("@0", Path.Combine(dir, "serial.js")); break;
+                        case '2': codeRawArray[i].Replace("@0", Path.Combine(dir, "plugins", "export", "export.min.js")); break;
+                        case '3': codeRawArray[i].Replace("@0", Path.Combine(dir, "plugins", "export", "export.css")); break;
+                        case '4': codeRawArray[i].Replace("@0", Path.Combine(dir, "themes", "light.js")); break;
+                        case '5': codeRawArray[i].Replace("@0", brightness[0].ToString()); break;
+                        case '6': codeRawArray[i].Replace("@0", brightness[1].ToString()); break;
+                        case '7': codeRawArray[i].Replace("@0", brightness[2].ToString()); break;
+                        case '8': codeRawArray[i].Replace("@0", brightness[3].ToString()); break;
+                        case '9': codeRawArray[i].Replace("@0", brightness[4].ToString()); break;
+                        case 'A': codeRawArray[i].Replace("@0", brightness[5].ToString()); break;
+                    }
                 }
             }
-            MessageBox.Show(codeRawArray[0]);
-
-            string[] arraya = { "a", "b" };
-            return arraya;
-            //return lines;
+            return codeRawArray;
         }
     }
 }
