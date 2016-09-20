5b5819bc5f42abb472e51a4354471eaf421c5d91
Dennis Kobert <d-kobert@web.de>
2016-09-20 20:24:44 +0200

    sorry noch n commit zu 42

--- LightControll/LightControll/Form1.cs
+++ LightControll/LightControll/Form1.cs
@@ -39,7 +39,7 @@ namespace LightControl
 
                         if (myport.ReadBufferSize > 0)
                         {
-                            if (myport.ReadChar() == '*')
+                            if (myport.ReadByte() == 42)
                             {
                                 return;
                             }
